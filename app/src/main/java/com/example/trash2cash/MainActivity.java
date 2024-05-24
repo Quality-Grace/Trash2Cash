61997450ed0c39e78c55e295f1b62c58a1e96a81
giorgos_spinkl <ics22084@uom.edu.gr>
2024-05-24 17:06:46 +0300

    Added a screen for the rewards and changed some ui stuff in the reward_settings_recycler_view.xml

--- app/src/main/java/com/example/trash2cash/MainActivity.java
+++ app/src/main/java/com/example/trash2cash/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.trash2cash.RecyclabeMaterialSettings.RecyclableMaterialSettingsActivity;
+import com.example.trash2cash.RewardScreen.RewardsActivity;
 import com.example.trash2cash.RewardSettings.RewardSettingsActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +19,6 @@ public class MainActivity extends AppCompatActivity {
         // Temp button to navigate to the RewardSettingsActivity
         findViewById(R.id.RecyclableMateriaSettingsTempButton).setOnClickListener(view -> startActivity(new Intent(MainActivity.this, RecyclableMaterialSettingsActivity.class)));
         findViewById(R.id.RewardSettingsTempButton).setOnClickListener(view -> startActivity(new Intent(MainActivity.this, RewardSettingsActivity.class)));
+        findViewById(R.id.RewardsTempButton).setOnClickListener(view -> startActivity(new Intent(MainActivity.this, RewardsActivity.class)));
     }
 }
---
+++ app/src/main/java/com/example/trash2cash/RewardScreen/RewardsActivity.java
@@ -0,0 +1,43 @@
+package com.example.trash2cash.RewardScreen;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
+
+
+import com.example.trash2cash.R;
+import com.example.trash2cash.RewardList;
+
+
+public class RewardsActivity extends AppCompatActivity {
+    private final RewardList rewardList = new RewardList();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rewards);
+
+        RecyclerView availableForYouRecycler = findViewById(R.id.availableForYouRecycler);
+        initializeRecycler(availableForYouRecycler, rewardList, true);
+
+        RecyclerView otherRewardsRecycler = findViewById(R.id.otherRewardsRecycler);
+        initializeRecycler(otherRewardsRecycler, rewardList, false);
+    }
+
+    public void initializeRecycler(RecyclerView recycler, RewardList rewardList, Boolean available){
+        // Adds CardView components to the Recycler
+        RewardsRecyclerAdapter adapter = new RewardsRecyclerAdapter(this, rewardList, available);
+        recycler.setAdapter(adapter);
+
+        // Sets a vertical layout for the Recycler
+        recycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        // Adds snappy scrolling
+        SnapHelper helper = new LinearSnapHelper();
+        helper.attachToRecyclerView(recycler);
+    }
+}
